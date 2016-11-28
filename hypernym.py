00d9126c11d82f80dfd50c2e395e9d24742bc59f
Superkebabbie <joostdoornkamp@hotmail.com>
2016-11-28 14:40:53 +0100

    Added grounded relations parsing and patched for mutliple syntaxes for sense names with spaces

--- hypernym.py
+++ hypernym.py
@@ -10,7 +10,6 @@ def splitAsList(string,openingbracket,closingbracket):
     #return the list of substringings surrounded by one of the brackets, respecting nested lists.
     substrings = []
     level = 0
-    
     for idx in range(len(string)):
         c = string[idx]
         if c == openingbracket:
@@ -21,22 +20,41 @@ def splitAsList(string,openingbracket,closingbracket):
             level -= 1
             if level == 0:
                 substrings.append(string[start+1:idx])
+    return(substrings)
     
+def splitIgnoringSublists(string,sep,openingbracket,closingbracket):
+    #splits string on sep, but not if sep occurs in a sublist (enclosed in brackets)
+    substrings = []
+    level = 0
+    start = 0
+    for idx in range(len(string)):
+        c = string[idx]
+        if level == 0:
+            if c == openingbracket:
+                level += 1
+            elif c == sep:
+                substrings.append(string[start:idx+1])
+                start = idx+1
+        else:
+            if c == closingbracket:
+                level -= 1
+    substrings.append(string[start:idx+1])
     return(substrings)
 
 def strToList(string):
     #convert a string representation of a list to an actual list, making the elements strings
     assert string.startswith('[') and string.endswith(']')
-    els = string[1:len(string)-1].split(',')
+    els = splitIgnoringSublists(string[1:len(string)-1],',','(',')')
     return els
     
 def modelToWordnet(sense):
     #turn sense names from the model syntax (pos_name_num) into the wordnet syntax (name.pos.num)
     sense = sense.strip('\'')
     parts = sense.split('_')
-    parts[1] = parts[1].replace(' ','_')
-    parts[0], parts[1] = parts[1], parts[0]
-    return '.'.join(parts)
+    pos = parts[0]
+    sense = '_'.join([w.replace(' ','_') for w in parts[1:len(parts)-1]])
+    num = parts[len(parts)-1]
+    return '.'.join([sense,pos,num])
     
 def wordnetToModel(sense):
     #turn sense names from the wordnet syntax (name.pos.num) into the model syntax (pos_name_num)
@@ -60,30 +78,32 @@ def loadModel(path):
     print(basemodel)
     basemodel = basemodel.lstrip('model(')
     basemodel = basemodel.rstrip(').')
-    stripped = splitAsList(basemodel,'[',']')#we now have the list of objects and the relations
+    stripped = splitAsList(basemodel,'[',']')#we now have the list of objects and the relations (and optionally the grounded relations)
     objects = stripped[0].split(',')
     print(objects)
-    relations = re.findall('f\(.+?\)',stripped[1])
+    relations = splitAsList(stripped[1],'(',')')
     reldict = OrderedDict()
     ignoredRels = []
     for r in relations:
-        r = splitAsList(r,'(',')')[0]
-        r = r.split(',')
-        r[2] = ','.join(r[2:])#bit of a hardcoded fix to prevent splitting on internal comma's, maybe add more syntax checks?
-        r = r[:3]
-        if r[0] == '1':#filter out relations with other arities
-            reldict[modelToWordnet(r[1])] = set(strToList(r[2]))#also converts _ (mod) to . (wordnet)
+        r = r.split(',',2)
+        print(r)
+        if r[0] == '1':
+            reldict[modelToWordnet(r[1])] = set(strToList(r[2]))
         else:
-            ignoredRels.append(r)
+            ignoredRels.append(r)   
     print(reldict)
     print(ignoredRels)
-    return(objects,reldict,ignoredRels)
+    grounds = None
+    if len(stripped) > 2:
+        grounds = splitAsList(stripped[2],'(',')')
+    print(grounds)
+    return(objects,reldict,ignoredRels,grounds)
 
 def listToStr(list):
     #variation on the normal str(list) function
     return '[' + ','.join(list) + ']'
     
-def saveModel(path,objects,reldict,ignoredRels):
+def saveModel(path,objects,reldict,ignoredRels,grounds):
     #save a (edited) model back to a file
     s = ["model("] #using lists as a stringBuilder substitute
     s.append(listToStr(objects))
@@ -99,6 +119,8 @@ def saveModel(path,objects,reldict,ignoredRels):
         s.append(",\n\t f(1," + wordnetToModel(r) + ',' + listToStr(sorted(reldict[r])) + ")")
     for r in ignoredRels:
         s.append(",\n\t f(" + ','.join(r) + ")")
+    if grounds != None:
+        s.append("],\n\t[g(" + '),\n\t g('.join(grounds) + ')')
     s.append("]).")
     file = open(path,'w')
     file.write(''.join(s))
@@ -133,7 +155,6 @@ def expandModel(relations):
         hypernyms = findAllHypernyms(wn.synset(sense))#returns all hypernyms, as synsets
         for h in hypernyms:
             addToRelations(newRelations,objects,h.name())
-    print(newRelations)
     return(newRelations)
         
 
@@ -141,6 +162,6 @@ def expandModel(relations):
 ## Main ##
 ##########
 
-(os,rel,ign) = loadModel('week1/cat-1022999_640.mod')
+(os,rel,ign,grs) = loadModel('grim/data/animal-196172_640.mod')
 rel = expandModel(rel)
-saveModel('output.mod',os,rel,ign)
+saveModel('output.mod',os,rel,ign,grs)
