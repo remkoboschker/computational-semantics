cf6c2b363a9a55e98f4ee4c7b592910bd1f79196
GGKorn <gunnar_gerstenkorn@hotmail.de>
2017-01-24 18:15:10 +0100

    ranking.py outline improved

--- project2/modelRanking.py
+++ project2/modelRanking.py
@@ -1,5 +1,17 @@
 
-from math import log
+from hypernym import loadModel, reduceModel
+from nltk import word_tokenize
+from nltk import stopwords
+from nltk.corpus import wordnet as wn
+from nltk.tag import StanfordPOSTagger
+_path_to_model = './stanford-postagger-2014-08-27/models/english-bidirectional-distsim.tagger'
+_path_to_jar ='./stanford-postagger-2014-08-27/stanford-postagger.jar'
+
+
+
+#from math import log
+
+DISPLAY = 10
 
 def rankModels(model,query):
     """
@@ -29,10 +41,16 @@ def rankModels(model,query):
     """
     coocurence = load(coocurence)
     cooc = coocurence = preproCooc()
-    proQuery = preproQuery()
     proModels = preproModel()
+    qTokens = preproQuery()
+    proQuery = findCommonReading(qTokens,proModels)
+    typical = typicality(proModels,proQuery,cooc)
+    ranking = sort(typical)
 
-
+    if len(ranking)>0:
+        return ranking[0:min([DISPLAY,len(ranking)])]
+    else:
+        debug("No fitting models found.")
 
 def typicality(model,query,coocurenceMatrix):
     """ cosine similarity of the model to all query terms in their 'normal' context as vector space model
@@ -49,8 +67,8 @@ def typicality(model,query,coocurenceMatrix):
     """
     weights = []
     for wq, qTerm in getWeights(query, cooc, weight = "df"):
-        innerTyp = 1 if innerTypicality == False else innerTypicality(model,cooc, weight = "tf")
-        for wm, mTerm in getWeights(model):
+        #innerTyp = 1 if innerTypicality == False else innerTypicality(model,cooc)
+        for wm, mTerm in getWeights(model, weight = "tf"):
             tf-idf = (wm * tf_m(model)) * (wq * idf_q(coocurenceMatrix[qTerm]))
             #weights.append(wm * qm * coocurenceMatrix[qTerm][mTerm] * innerTyp)
     score = sum(weights)
@@ -59,26 +77,19 @@ def typicality(model,query,coocurenceMatrix):
 
 
 
-def
 
-    F AST C OSINE S CORE ( q )
-1 float Scores [ N ] = 0
-2 for each d
-3 do Initialize Length [ d ] to the length of doc d
-4 for each query term t
-5 do calculate w t,q and fetch postings list for t
-6
-for each pair ( d, tf t,d ) in postings list
-7
-do add wf t,d to Scores [ d ]
-8 Read the array Length [ d ]
-9 for each d
-10 do Divide Scores [ d ] by Length [ d ]
-11 return Top K components of Scores []
+def getWeights(term,coocurenceMatrix, weight = 1):
+    """ Weights the
+    INPUT:
+        @coocurenceMatrix:      a dictionary:
+        @weight:                a float: impact of the found term, indicating it's closeness to the original concept
+    OUTPUT:
+        @weight:                a float: impact of the found term, indicating it's closeness to the original concept
+        @term:                  a wordnet sense: closest sense found in the @coocurenceMatrix
+                # reconsider if @coocurenceMatrix should be the orientation. probably needs to be
+    """
 
-def getWeights(term1,term2,coocurenceMatrix, weight = 1):
-
-    if weight <= 0.05: # or if entity
+    if weight <= 0.05 or term == "entity.n.1": # or if entity
         return 0
     elif mTerm in coocurenceMatrix:
         return coocurenceMatrix[term1,term2]
@@ -89,7 +100,8 @@ def getWeights(term1,term2,coocurenceMatrix, weight = 1):
 
 
 
-
+# Done
+################################################################################
 def innerTypicality(context, coocurenceMatrix, log = True, normalize = True ):
     """
     'gives reciproce frequency of coocurences in @context relative to number of terms in context'
@@ -113,10 +125,32 @@ def innerTypicality(context, coocurenceMatrix, log = True, normalize = True ):
 
     return x
 
+def preproModel(model):
+    """ preprocesses the model into its basic terms using only the synsets in the model
+        'assumption: all models are already true for the query, therefor no more explicit parsing is relevat here'
+        INPUT:
+            @query:     a string: the nlp string of the query
+        OUTPUT:
+            @return:    a list: the synsets of each non stop word from the query
+    """
+
+    x = hypernym.reduceModel(hypernym.loadModel("data/" +  model + ".mod")[1])
+    synsets = wn.synsets(list(x[0].keys())[i][0:-5])    # should we actually reduce the model? Yes, since we need the clearest
+
+    senses = []
+    for a in modelDic.keys():
+        for b in range(0,len(modelDic[a])):
+            senses.append(str(a))
+
+    return senses
+
+################################################################################
+
+# TODO: """"""
 
 def preproCooc(coocurenceMatrix, normalize = True):
-    """
-    'preprocesses the coocurenceMatrix into a posting list representation'
+    """ Preprocesses the coocurenceMatrix into a posting list representation. The entries are wordnet senses.
+        # which will be the biggest problem (so far) as we can't validate them. approach so far: just add all senses to matrix
     INPUT:
         @coocurenceMatrix:  a dictionary:
         @normalize:         a boolean:
@@ -139,21 +173,53 @@ def preproCooc(coocurenceMatrix, normalize = True):
 
 def preproQuery(query):
     """
-    'preprocesses the query into its basic terms, removing stopwords and finding synsets'
+    'preprocesses the query into its basic terms, removing stopwords and finding senses'
     'assumption: all models are already true for the query, therefor no more explicit parsing is relevat here'
     INPUT:
         @query:     a string: the nlp string of the query
     OUTPUT:
         @return:    a list: the synsets of each non stop word from the query
     """
-    project1.getSynset()
-def preproModel(model):
+
+    qTokens = word_tokenize(line)
+    qTokens = [token for token in qTokens if token not in STOPWORDS]
+
+    # project1.getSynset()
+    return qTokens
+
+
+
+def findCommonReading(queryTerms,modelSynsets):
+    """ for disambiguation of query terms: finds a common reading in the model (which has ground truth/gold label annotation)
+        INPUT:
+            @modelSynsets:      a list: gold label senses from the model
+            @queryTerms:        a list: terms from the query that need to bee maped to synset meanings
+        OUTPUT:
+            @return:            a list: senses for which there were common readings - else None
     """
-    'preprocesses the model into its basic terms using only the synsets in the model'
-    'assumption: all models are already true for the query, therefor no more explicit parsing is relevat here'
-    INPUT:
-        @query:     a string: the nlp string of the query
-    OUTPUT:
-        @return:    a list: the synsets of each non stop word from the query
+
+def cosine(vectorA,vectorB):
+    """    F AST C OSINE S CORE ( q )
+    1 float Scores [ N ] = 0
+    2 for each d
+    3 do Initialize Length [ d ] to the length of doc d
+    4 for each query term t
+    5 do calculate w t,q and fetch postings list for t
+    6
+    for each pair ( d, tf t,d ) in postings list
+    7
+    do add wf t,d to Scores [ d ]
+    8 Read the array Length [ d ]
+    9 for each d
+    10 do Divide Scores [ d ] by Length [ d ]
+    11 return Top K components of Scores []
     """
-    project1.getSynset()
+
+##########
+## MAIN ##
+##########
+
+if __name__ == "__main__":
+
+    sys.stdin
+    rankModels(model,query)
