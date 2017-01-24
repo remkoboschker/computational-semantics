700bad6cdbd5f3452dd5463c8b7b831f9a7f657f
Remko Boschker <remkoboschker@RN-145-97-129-249.eduroam.rug.nl>
2017-01-24 16:22:18 +0100

    findImages working in batch mode and outputting html

--- project2/findImages.sh
+++ project2/findImages.sh
@@ -3,7 +3,7 @@ source ../venv/bin/activate
 
 find() {
   sentence=$1
-  #echo ${sentence}
+  echo ${sentence}
   sentence_with_most_specific_hypernym=`python3 grimSearch0.py "${sentence}"`
   #echo ${sentence_with_most_specific_hypernym}
   first_order_semantic_representation=`swipl -g "[prolog/semdcg],parse_and_write('${sentence_with_most_specific_hypernym}'),halt."`
@@ -16,32 +16,59 @@ find() {
   five_most_relevant_models=`python3 rank.py '${sentence_with_most_specific_hypernym}' ${models_that_satisfy_first_order_semantic_representation}`
   #echo ${five_most_relevant_models}
   images=${five_most_relevant_models//mod/jpg}
+  echo ${images}
   document=`
-  echo "\\documentclass{article}"
-  echo "\\usepackage{graphicx}"
-  echo "\\begin{document}"
-  echo "\\begin{quote}"
-  echo "${sentence}"
-  echo "\\end{quote}"
-  echo "\\begin{quote}"
-  echo "${sentence_with_most_specific_hypernym}"
-  echo "\\end{quote}"
-  echo "\\begin{quote}"
-  echo "\\verb+${first_order_semantic_representation}+"
-  echo "\\end{quote}"
+  # echo "\\documentclass{article}"
+  # echo "\\usepackage{graphicx}"
+  # echo "\\begin{document}"
+  # echo "\\begin{quote}"
+  # echo "${sentence}"
+  # echo "\\end{quote}"
+  # echo "\\begin{quote}"
+  # echo "${sentence_with_most_specific_hypernym}"
+  # echo "\\end{quote}"
+  # echo "\\begin{quote}"
+  # echo "\\verb+${first_order_semantic_representation}+"
+  # echo "\\end{quote}"
+  # for image in ${images}; do
+  #   echo "\\includegraphics[width=120pt,height=90pt]{${image}}"
+  # done
+  # echo "\\end{document}"
+  echo "<html><body><h1>${sentence}<h2>${sentence_with_most_specific_hypernym}<ul>"
   for image in ${images}; do
-    echo "\\includegraphics[width=120pt,height=90pt]{${image}}"
+    echo "<li><img src=\"../${image}\"></li>"
   done
-  echo "\\end{document}"
   `
-  pdflatex -jobname=${sentence// /-} -output-directory=./results ${document}
+  echo "${document}" > "./results/${sentence// /-}.html"
+  #pdflatex -jobname=${sentence// /-} -output-directory=./results -interaction=batchmode ${document}
 }
 
 if [ -f "$1" ]
 then
   while read -r line
   do
-    find "${line}"
+    sentence=${line//[$'\r'$'\n']/}
+    # ` echo "${line}" | tr -d '\n\r'`
+    sentence_with_most_specific_hypernym=`python3 grimSearch0.py "${sentence}"`
+    #echo ${sentence_with_most_specific_hypernym}
+    first_order_semantic_representation=`swipl -g "[prolog/semdcg],parse_and_write('${sentence_with_most_specific_hypernym}'),halt."`
+    #echo ${first_order_semantic_representation}
+    models_that_satisfy_first_order_semantic_representation=$(
+    for model in `ls ../data/*.mod`; do
+        echo `swipl -g "[prolog/model_checker],main_satisfying(${first_order_semantic_representation},'${model}'),halt."`
+    done)
+    #echo ${models_that_satisfy_first_order_semantic_representation}
+    five_most_relevant_models=`python3 rank.py '${sentence_with_most_specific_hypernym}' ${models_that_satisfy_first_order_semantic_representation}`
+    #echo ${five_most_relevant_models}
+    images=${five_most_relevant_models//mod/jpg}
+    echo ${images}
+    document=`
+    echo "<html><body><h1>${line}<h2>${sentence_with_most_specific_hypernym}<ul>"
+    for image in ${images}; do
+      echo "<li><img src=\"../${image}\"></li>"
+    done
+    `
+    echo "${document}" > "./results/${sentence// /-}html"
   done < $1
 else
   find "$1"
