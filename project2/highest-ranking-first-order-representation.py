06f8b3d7f5eb2cc4f80ae59a1f73e507a16c0a8a
Joost Doornkamp <Joost Doornkamp>
2017-01-26 16:24:02 +0100

    fixed script not being able to return multiple representations

--- project2/highest-ranking-first-order-representation.py
+++ project2/highest-ranking-first-order-representation.py
@@ -1,5 +1,13 @@
 import sys
+import re
+from collections import OrderedDict
 
-models = []
-for line in sys.stdin: models.append(line)
-sys.stdout.write(models[0])
+models = {}
+
+for model in sys.stdin:
+    model = model.rstrip('\n')
+    score = len(re.findall("[nsa]_[a-z]+(?:_\d+)?\([\w,]+\)",model))#finds all terms
+    models[model] = score
+    
+models = OrderedDict(sorted(models.items(),key=lambda m:m[1],reverse=True))
+sys.stdout.write('\n'.join(models.keys()))
--- project2/prolog/semdcg.pl
+++ project2/prolog/semdcg.pl
@@ -45,12 +45,15 @@ parse_from_file(FileIn,FileOut):-
 parse_and_write(Atom):-
   downcase_atom(Atom,Down),
   tokenize_atom(Down,Tokens),
-  s(Sem,Tokens,[]),
-  betaConvert(Sem,Red),
-  numbervars(Red,23,_),
-  write(Red),
-  nl,
-  fail; true.
+  findall(Red,(
+    s(Sem,Tokens,[]),
+    betaConvert(Sem,Red),
+    numbervars(Red,23,_)
+  ),L),
+  writeall(L).
+  
+writeall([]).
+writeall([H|T]) :- write(H),nl,writeall(T).
 
 
 /* ------------------------------------------------
